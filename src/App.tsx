7b3200cdfe99d2153e81f087e41f588ef3a0345c
황민서 <102010606+allen8524@users.noreply.github.com>
2026-06-08 12:23:41 +0900

    feat: 개발 역량 상세 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
 import ProjectDetailPage from "./pages/ProjectDetailPage";
+import ServiceDetailPage from "./pages/ServiceDetailPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects/:projectId" element={<ProjectDetailPage />} />
+        <Route path="/services/:serviceId" element={<ServiceDetailPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </HashRouter>
