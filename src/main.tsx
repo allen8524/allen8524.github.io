37aece67c65bfde75dcc7f0d816fbe4f85ce42d7
황민서 <102010606+allen8524@users.noreply.github.com>
2026-06-09 16:22:13 +0900

    style: 활동 및 경험 목록 스타일 적용

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import "./styles/project-detail.css";
 import "./styles/project-detail-tuning.css";
 import "./styles/service-detail.css";
 import "./styles/service-detail-tuning.css";
+import "./styles/resume-tuning.css";
 import "./styles/troubleshooting.css";
 
 const rootElement = document.getElementById("root");
