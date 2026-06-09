5d68786f959cb37b6dbef996b0598b62c1c1234d
황민서 <102010606+allen8524@users.noreply.github.com>
2026-06-09 16:53:24 +0900

    fix: 프로젝트 갤러리 가운데점 공백 정규화

--- src/components/project-detail/ProjectGallery.tsx
+++ src/components/project-detail/ProjectGallery.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ImagePreview from "../portfolio/ImagePreview";
 import type { ProjectDetail } from "../../types/project";
+import { formatMiddleDotSpacing } from "../../utils/typography";
 
 type ProjectGalleryProps = {
   detail: ProjectDetail;
@@ -22,22 +23,26 @@ function ProjectGallery({ detail }: ProjectGalleryProps) {
         <p>기존 상세 페이지에서 사용하던 대표 화면 자료를 공통 상세 페이지 안에 정리했습니다.</p>
       </div>
       <div className="project-gallery-grid">
-        {detail.gallery.map((image) => (
-          <figure className="project-gallery-item" key={image.src}>
-            <button
-              type="button"
-              className="project-detail-image-button"
-              aria-label={`${image.alt} 이미지 확대`}
-              onClick={() => setPreviewImage(image)}
-            >
-              <img src={image.src} alt={image.alt} className="img-fluid" loading="lazy" />
-            </button>
-            <figcaption>{image.alt}</figcaption>
-          </figure>
-        ))}
+        {detail.gallery.map((image) => {
+          const imageAlt = formatMiddleDotSpacing(image.alt);
+
+          return (
+            <figure className="project-gallery-item" key={image.src}>
+              <button
+                type="button"
+                className="project-detail-image-button"
+                aria-label={`${imageAlt} 이미지 확대`}
+                onClick={() => setPreviewImage(image)}
+              >
+                <img src={image.src} alt={imageAlt} className="img-fluid" loading="lazy" />
+              </button>
+              <figcaption>{imageAlt}</figcaption>
+            </figure>
+          );
+        })}
       </div>
       {previewImage && (
-        <ImagePreview alt={previewImage.alt} image={previewImage.src} onClose={() => setPreviewImage(null)} />
+        <ImagePreview alt={formatMiddleDotSpacing(previewImage.alt)} image={previewImage.src} onClose={() => setPreviewImage(null)} />
       )}
     </section>
   );
